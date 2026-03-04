009a8640fcb7dae661f4469790660050acb651fd
vmdivyareddy18 <vmdivyareddy18@gmail.com>
2026-03-04 22:40:11 +0530

    update

--- sumof2numbers.c
+++ sumof2numbers.c
@@ -2,5 +2,11 @@
 int main()
 {
     int x = 0, y = 0, sum = 0;
+    printf("Enter the first number: ");
+    scanf("%d", &x);
+    printf("Enter the second number: ");
+    scanf("%d", &y);
+    sum = x + y;
+    printf("The sum of %d and %d is %d\n", x, y, sum);
     return 0;
 }
