a47b7311936893f8a0b45bfdfd6177538a3b39f2
vmdivyareddy18 <vmdivyareddy18@gmail.com>
2026-03-04 22:33:15 +0530

    update

--- cube.c
+++ cube.c
@@ -2,5 +2,8 @@
 int main()
 {
     int number;
-    printf("Enter a number;");
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    printf("The cube of %d is %d\n", number, number * number * number);
+    return 0;
 }
